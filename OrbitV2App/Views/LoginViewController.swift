3e57241b954c79f9d958cf4cbb162a391e6538bf
Sam <dyerrate@gmail.com>
2023-06-08 17:51:25 -0500

    Committing the update with the following:  - Completed home screen(almost)  - Updated the data base to word and load the solar system

--- OrbitV2App/Views/LoginViewController.swift
+++ OrbitV2App/Views/LoginViewController.swift
@@ -17,136 +17,42 @@ class LoginViewController: UIViewController, ASAuthorizationControllerDelegate, 
     private let loginButton = UIButton(type: .system)
     private let forgotPasswordButton = UIButton(type: .system)
     private let registerButton = UIButton(type: .system)
+    var animationTimer: Timer?
 
     override func viewDidLoad() {
+        //        animationTimer = Timer.scheduledTimer(timeInterval: 6.0, target: self, selector: #selector(self.runRandomAnimation), userInfo: nil, repeats: true)
+        
         super.viewDidLoad()
-        setupUI()
+        setupBackgroundImage()
+        let (_, orbitLabel) = setupMyOrbitTitle()
+        setupSpaceThemeAppleSignInButton(below: orbitLabel)
+        setupInfoButton()
     }
-
-    private func setupUI() {
-        view.backgroundColor = UIColor.orbitBackground
-        // Configure emailTextField
-          emailTextField.backgroundColor = UIColor.orbitTextFieldBackground
-          emailTextField.layer.cornerRadius = 5
-          emailTextField.layer.borderWidth = 1
-          emailTextField.layer.borderColor = UIColor.systemGray5.cgColor
-          emailTextField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
-          emailTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: emailTextField.frame.height))
-          emailTextField.leftViewMode = .always
-
-          // Configure passwordTextField
-          passwordTextField.backgroundColor = UIColor.orbitTextFieldBackground
-          passwordTextField.layer.cornerRadius = 5
-          passwordTextField.layer.borderWidth = 1
-          passwordTextField.layer.borderColor = UIColor.systemGray5.cgColor
-          passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
-          passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: passwordTextField.frame.height))
-          passwordTextField.leftViewMode = .always
-
-          // Configure loginButton
-        loginButton.backgroundColor = UIColor.orbitLoginButtonBackground
-        loginButton.layer.cornerRadius = 5
-        loginButton.layer.shadowColor = UIColor.black.cgColor
-        loginButton.layer.shadowOffset = CGSize(width: 0, height: 3)
-        loginButton.layer.shadowOpacity = 0.2
-        loginButton.layer.shadowRadius = 5
-        loginButton.setTitle("Login", for: .normal)
-        loginButton.setTitleColor(.white, for: .normal)
-        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
-        
-          // Configure forgotPasswordButton
-        forgotPasswordButton.setTitle("Forgot Password?", for: .normal)
-        forgotPasswordButton.setTitleColor(UIColor.orbitForgotPasswordButtonTitle, for: .normal)
-        forgotPasswordButton.addTarget(self, action: #selector(forgotPassword), for: .touchUpInside)
-        registerButton.setTitle("Register", for: .normal)
-        registerButton.setTitleColor(UIColor.orbitForgotPasswordButtonTitle, for: .normal)
-        registerButton.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
-        registerButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        let logoImageView = UIImageView(image: UIImage(named: "OrbitLoginLogo"))
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(logoImageView)
-        let spacerView = UIView()
-        spacerView.translatesAutoresizingMaskIntoConstraints = false
-        spacerView.heightAnchor.constraint(equalToConstant: 10).isActive = true
-        // Add constraints
-        
-        // Container view for email, password, and login button
-        let containerView = UIView()
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        containerView.backgroundColor = .systemGray6
-        containerView.layer.cornerRadius = 10
-        containerView.layer.shadowColor = UIColor.black.cgColor
-        containerView.layer.shadowOffset = CGSize(width: 0, height: 3)
-        containerView.layer.shadowOpacity = 0.2
-        containerView.layer.shadowRadius = 5
-        
-        view.addSubview(containerView)
-        NSLayoutConstraint.activate([
-            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            containerView.heightAnchor.constraint(equalToConstant: 230)
-        ])
-        let appleSignInButton = ASAuthorizationAppleIDButton()
-        appleSignInButton.addTarget(self, action: #selector(handleAppleSignInButton), for: .touchUpInside)
-        appleSignInButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(appleSignInButton)
-        NSLayoutConstraint.activate([
-            appleSignInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            appleSignInButton.topAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 20),
-        ])
-        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton, forgotPasswordButton, registerButton])
-         stackView.axis = .vertical
-         stackView.spacing = 12
-         stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        containerView.addSubview(stackView)
-        NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
-            stackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 16),
-            stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -16)
-            ])
-
-
-        // Add constraints
-        NSLayoutConstraint.activate([
-            
-            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            logoImageView.bottomAnchor.constraint(equalTo: containerView.topAnchor, constant: -30),
-            logoImageView.widthAnchor.constraint(equalToConstant: 150), // Set the desired width
-            logoImageView.heightAnchor.constraint(equalToConstant: 150),
-            
-        ])
-                                    
-    }
+//    deinit {
+//        // Invalidate timer when view controller is deallocated to prevent memory leaks
+//        animationTimer?.invalidate()
+//    }
     
+    
+    func setupInfoButton() {
+        let infoButton = UIButton(type: .system)
+        let infoIcon = UIImage(systemName: "questionmark.circle")
+        infoButton.setImage(infoIcon, for: .normal)
+        infoButton.tintColor = UIColor.gray.withAlphaComponent(0.5)
+        infoButton.addTarget(self, action: #selector(handleInfoButton), for: .touchUpInside)
+        infoButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(infoButton)
 
-    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            emailTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            emailTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            emailTextField.heightAnchor.constraint(equalToConstant: 44),
-
-            passwordTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            passwordTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
-            passwordTextField.heightAnchor.constraint(equalToConstant: 44),
-
-            loginButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            loginButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
-            loginButton.heightAnchor.constraint(equalToConstant: 44),
-
-            forgotPasswordButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            forgotPasswordButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            forgotPasswordButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 10),
-            forgotPasswordButton.heightAnchor.constraint(equalToConstant: 44)
-            
+            infoButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            infoButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
         ])
     }
+    @objc func handleInfoButton() {
+        // Display info panel about the app
+    }
+
+    
     @objc func handleAppleSignInButton() {
         let request = ASAuthorizationAppleIDProvider().createRequest()
         request.requestedScopes = [.fullName, .email]
@@ -154,6 +60,64 @@ class LoginViewController: UIViewController, ASAuthorizationControllerDelegate, 
         controller.delegate = self
         controller.presentationContextProvider = self
         controller.performRequests()
+    }
+    func setupBackgroundImage() {
+        // Create UIImageView
+        let backgroundImageView = UIImageView()
+        
+        // Set the image property to 'backgroundImage' from assets
+        backgroundImageView.image = UIImage(named: "backgroundImage")
+        backgroundImageView.contentMode = .scaleAspectFill
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    func setupSpaceThemeAppleSignInButton(below titleLabel: UILabel) {
+        let appleSignInButton = ASAuthorizationAppleIDButton()
+        appleSignInButton.addTarget(self, action: #selector(handleAppleSignInButton), for: .touchUpInside)
+        appleSignInButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(appleSignInButton)
+        NSLayoutConstraint.activate([
+            appleSignInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            appleSignInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: view.frame.height / 3.7),
+            appleSignInButton.widthAnchor.constraint(equalToConstant: 200),
+            appleSignInButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    func setupMyOrbitTitle() -> (myLabel: UILabel, orbitLabel: UILabel) {
+        let myLabel = UILabel()
+        myLabel.text = "My"
+        myLabel.font = UIFont(name: "StarcruiserExpandedSemi-Italic", size: 75)
+        myLabel.textColor = .white
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(myLabel)
+
+        let orbitLabel = UILabel()
+        orbitLabel.text = "Orbit"
+        orbitLabel.font = UIFont(name: "StarcruiserExpandedSemi-Italic", size: 75)
+        orbitLabel.textColor = .white
+        orbitLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(orbitLabel)
+        //NOTE: Remove comments to print fonts
+//        for family in UIFont.familyNames {
+//            for name in UIFont.fontNames(forFamilyName: family) {
+//                print(name)
+//            }
+//        }
+        NSLayoutConstraint.activate([
+            orbitLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            orbitLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -view.frame.height / 3.5),
+            myLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myLabel.bottomAnchor.constraint(equalTo: orbitLabel.topAnchor, constant: 17) // Adjust this constant value to change the spacing
+        ])
+
+        return (myLabel, orbitLabel)
     }
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
@@ -186,31 +150,7 @@ class LoginViewController: UIViewController, ASAuthorizationControllerDelegate, 
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         // Handle error.
     }
-    
-    @objc func loginButtonTapped() {
-        UIView.animate(withDuration: 0.1, animations: {
-            self.loginButton.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-        }) { _ in
-            UIView.animate(withDuration: 0.1, animations: {
-                self.loginButton.transform = CGAffineTransform.identity
-            }) { _ in
-                // Perform your login action here
-//                self.loginUser()
-            }
-        }
-        emailTextField.isHidden = true
-        loginButton.isHidden = true
-        forgotPasswordButton.isHidden = true
-        passwordTextField.isHidden = true
-        registerButton.isHidden = true
-    }
 
-    @objc func registerButtonTapped() {
-        let registerVC = RegisterViewController()
-        let navigationController = UINavigationController(rootViewController: registerVC)
-        navigationController.modalPresentationStyle = .fullScreen
-        present(navigationController, animated: true, completion: nil)
-    }
 
     private func showLoadingScreen(for user: User) {
         let loadingViewController = LoadingViewController()
@@ -241,24 +181,53 @@ class LoginViewController: UIViewController, ASAuthorizationControllerDelegate, 
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
-    private func showLoginError(error: Error) {
-        let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(okAction)
-        present(alertController, animated: true, completion: nil)
-    }
-
-    @objc private func forgotPassword() {
-        // Handle forgot password functionality
-        if let url = URL(string: "https://www.sirdyer.com") {
-            let safariVC = SFSafariViewController(url: url)
-            self.present(safariVC, animated: true, completion: nil)
-        }
-    }
-
     
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
         return self.view.window!
     }
+    
+    //~~~Animations~~~
+    
+//    @objc func runRandomAnimation() {
+//        //["meteor", "spaceship", "satellite"]
+//        let animations = ["meteor"]
+//        let randomAnimation = animations.randomElement()
+//
+//        switch randomAnimation {
+//        case "meteor":
+//            runMeteorAnimation()
+////        case "spaceship":
+////            runSpaceshipAnimation()
+////        case "satellite":
+////            runSatelliteAnimation()
+//        default:
+//            break
+//        }
+//    }
+//    func runMeteorAnimation() {
+//        // Create an UIImageView for your meteor
+//        let meteor = UIImageView(image: UIImage(named: "meteor1"))
+//        let randomStartY = randomYValue()
+//        let randomEndY = randomYValue()
+//
+//        // Start the meteor outside the screen on the right
+//        meteor.frame = CGRect(x: self.view.frame.maxX + 100, y: randomStartY, width: 100, height: 100)
+//
+//        // Add it to your view
+//        self.view.addSubview(meteor)
+//
+//        // Animate the meteor flying from right to left
+//        UIView.animate(withDuration: 2.0, animations: {
+//            // End the meteor outside the screen on the left
+//            meteor.frame = CGRect(x: -100, y: randomEndY, width: 100, height: 100)
+//        }, completion: { _ in
+//            // Remove the meteor from the view after the animation completes
+//            meteor.removeFromSuperview()
+//        })
+//    }
+//    func randomYValue() -> CGFloat {
+//          return CGFloat(arc4random_uniform(UInt32(self.view.frame.maxY)))
+//      }
+    
 }
 
